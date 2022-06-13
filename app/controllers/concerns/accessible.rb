ccfbffbfaece3fa03c833cb201544f5861bd5ad3
Usama Ayub <usama.dar@devsinc.com>
2022-06-13 12:15:46 +0500

    removed rubocop warning

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -9,9 +9,9 @@ module Accessible
   protected
 
   def check_resource
-    if user_signed_in?
-      flash.clear
-      redirect_to(users_authenticated_root_path) and return
-    end
+    return unless user_signed_in?
+
+    flash.clear
+    redirect_to(users_authenticated_root_path) and return
   end
 end
