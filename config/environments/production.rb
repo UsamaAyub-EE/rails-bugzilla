36a914556fdd48d983adc26d624fb9198cbd9299
Usama Ayub <usama.dar@devsinc.com>
2022-06-17 14:19:37 +0500

    modified production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Mailer settings
-  config.action_mailer.default_url_options = { host: 'sleepy-sands-90281.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'warm-sierra-31625.herokuapp.com', protocol: 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'https://sleepy-sands-90281.herokuapp.com',
+    domain: 'https://warm-sierra-31625.herokuapp.com',
     user_name: Rails.application.credentials.dig(:google_smtp, :email),
     password: Rails.application.credentials.dig(:google_smtp, :password),
     authentication: 'plain',
